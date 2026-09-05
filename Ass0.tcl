e4c82332933bfdece3b0e975c0b50927510bda7c
Liibobobo <linhhihihuhu@gmail.com>
2026-09-05 18:31:23 +0800

    Logical Operator

--- Ass0.tcl
+++ Ass0.tcl
@@ -1,8 +1,12 @@
 puts "****Logical Operations****"
-set a 5
-set b -1
-set c 0
-set var0 [expr $a && $c ]
-set var1 [expr $a || $b ]
-puts "var0 value = $var0"
-puts "var1 value = $var1"
+set a 5 
+set b -1 
+set c 0 
+#6: Logical AND of a and c 
+set var0 [expr $a && $c]
+
+#7: Logical OR of a and b 
+set var1 [expr $a || $b]
+
+#8: display content of var0 and var1 
+puts "result and of a\&c: $var0 \nresult or of a\&b: $var1"
